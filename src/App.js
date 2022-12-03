45387fd1c3704c490273481dff511e07318f0a3a
TimothySingleton <t.singleton89@hotmail.com>
2022-12-02 19:43:27 -0700

    fix routing issues and make navbar dynamic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import './App.css';
 import Main from './components/Main';
-import Navbar from './components/Navbar';
-import {
-  // BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-// import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          <Navbar/>
-      </header>
-      <main>
-      <Routes>
-          <Route path='/' element={<Main/>}/>
-      </Routes>
-      </main>
-      {/* <Footer/> */}
+      <Main/>
+      
     </div>
   );
 }
