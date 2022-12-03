4b8faf9652beb0f5798b6313bcdf38b579510348
TimothySingleton <t.singleton89@hotmail.com>
2022-12-02 19:45:37 -0700

    working on server side rendering for projects

---
+++ src/components/pages/Projects/Projects.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import Box from '@mui/material/Box';
+import { Container } from '@mui/system'
+import MainLayout from '../../layouts/MainLayout'
+
+// const Item = styled(Paper)(({ theme }) => ({
+//     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//     ...theme.typography.body2,
+//     padding: theme.spacing(2),
+//     textAlign: 'center',
+//     color: theme.palette.text.secondary,
+// }));
+
+const Projects = () => {
+
+    const [title, setTitle] = useState("Test Title")
+    const [subTitle, setSubtitle] = useState("Lorem ipsss yyyyyyyy")
+    const [summary, setSummary] = useState("Lorem daskhjfgsdijgdklsgfjioejasdklnvirjgsdjfgdsgjiopr..")
+
+    return (
+        <Box
+        mt={10}
+        sx={{background: "gray",Height: 100}}>
+        <Container className="project-header">
+            <p class="the">The</p>
+            <h2 class="dod-rl wfd">Learning <span class="dod-of">As a</span> DEVELOPER</h2> 
+        </Container>
+        <MainLayout title={title}/>
+        </Box>
+    )
+}
+
+export default Projects
