c546074add421eb841e064bbc485dfb1e90a518b
TimothySingleton <t.singleton89@hotmail.com>
2022-12-30 15:57:12 -0700

    working with the MUI Grid layout

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -11,16 +11,18 @@ const About = () => {
         <>
         <Box 
             mt={10}
-            sx={{ display: "flex",  flexDirection: "column"}}>
+            sx={{ 
+                display: "flex",  
+                flexDirection: "column"}}>
             <Container className="about-header">
             <p class="the">The</p>
             <h2 class="dod-rl">Development <span class="dod-of">of a</span> DEVELOPER</h2> 
             </Container>
 
-            <Grid Container spacing={{ sm: 3, md: 3 }}columns={{ sm: 8, md: 12 }} 
+            <Grid Container spacing={{ sm: 2, md: 3 }}columns={{ sm: 8, md: 12 }} 
                 className="skill-card"
                 sx={{display:"flex",
-                p:5 }}>
+                p:1 }}>
                 <Grid Item xs={8} sm={12}>
                 <Card
                 elevation={4}
@@ -50,13 +52,14 @@ const About = () => {
                 </Card>
                 </Grid>
             </Grid>
+            
             <Box>
                 <div class="current-project">
                     <div class="project">
                         <h3 class="project-heading">Current Project</h3>
                         <p class="project-title">My Next Promotion App.</p>
                         <p class="project-info">A full stack CRUD application. This application was developed to assist the DoD firefighter  hiring process as well as  promotions evaluations. It uses data stored to analyze each individual candidate base on skill sets and qualifications. <br></br>
-                        <span>Technologies used: Python & Flask, Jinja2, HTML & CSS, Adobe Xd</span></p>
+                        <span>Technologies used: </span> <span className='stacks'>Python & Flask, Jinja2, HTML & CSS, Adobe Xd</span></p>
                     </div> 
                     <DisplayProjectCard/>
                 </div>
