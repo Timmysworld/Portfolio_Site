aad5553db9ccb95b296cab5a4d64d1131840d823
TimothySingleton <t.singleton89@hotmail.com>
2023-01-08 16:17:18 -0700

    updated contact page

--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -5,15 +5,35 @@ import { Container } from '@mui/system';
 
 const Contact = () => {
     return (
+        <>
         <Box
         mt={10}
-        sx={{background: "gray"}}>
+        sx={{}}>
         <Container className="contact-header">
             <p class="the">The</p>
             <h2 class="dod-rl wfd">Way <span class="dod-of">to hire a </span> DEVELOPER</h2> 
         </Container>
+        </Box>
+        <div className="c-form">
+        <div className="form-box-title">
+            <h2>
+                <span className='split-left'>
+                <span className='line-left'>LETS</span>
+                </span> 
+            </h2>
+            <h2>
+                <span className='split-right'>
+                <span className='line-right'>CONNECT</span>
+                </span>
+            </h2>
+        </div>
+        <div className="form_box">
+            
+        </div>
+        </div>
 
-    </Box>
+        </>
+        
     )
 }
 
